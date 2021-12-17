b2cda4fe2757ff77147cdbc555bacd73986d570d
virginie lty <virginielty@gmail.com>
2021-12-17 20:39:38 +0100

    controllers rake

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,22 +4,26 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
-
   def new
     @restaurant = Restaurant.new
   end
 
   def create
-    @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-
-    redirect to restaurant_path(@restaurant)
+  @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
-   private
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+    @restaurant
+  end
+
+  private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
